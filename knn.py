ba6a7b34e59959f9371f4a0e4bb94d8ac346dc7d
Murat Erdogan <murat-erdogan@users.noreply.github.com>
2016-04-22 02:01:38 +0100

    Create knn.py

---
+++ knn.py
@@ -0,0 +1,24 @@
+from sklearn.neighbors import KNeighborsClassifier
+import csv
+import numpy as np
+
+train = []
+labels = []
+test = []
+
+reader = csv.reader(open("train.csv", "r"), delimiter=",")
+next(reader)
+for row in reader:
+    labels.append(int(row[0]))
+    row = row[1:]
+    train.append(np.array(np.int64(row)))
+
+reader = csv.reader(open("test.csv", "r"), delimiter=",")
+next(reader)
+for row in reader:
+    test.append(np.array(np.int64(row)))
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn = knn.fit(train, labels)
+out = knn.predict(test)
+np.savetxt('out.csv', out, delimiter=',', fmt='%s')
