6d16aa5e00a0e4c352bbd02a733375b153de8e23
Murat Erdogan <murat-erdogan@users.noreply.github.com>
2021-09-17 09:47:48 +0100

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,33 @@
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train[:1000].reshape((X_train[:1000].shape[0], 28, 28, 1)).astype('float32')
+X_test = X_test[:100].reshape((X_test[:100].shape[0], 28, 28, 1)).astype('float32')
+X_train = X_train / 255
+X_test = X_test / 255
+y_train = np_utils.to_categorical(y_train[:1000])
+y_test = np_utils.to_categorical(y_test[:100])
+num_classes = y_test.shape[1]
+
+model = Sequential()
+model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(15, (3, 3), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=200)
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
